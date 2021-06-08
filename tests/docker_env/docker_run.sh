3bafbd4f5fc07531f557148b264ce6408c822e2f
PabloLec <pablo.lecolinet@gmail.com>
2021-06-08 15:17:53 +0200

    Work in progress on Docker env

--- tests/docker_env/docker_run.sh
+++ tests/docker_env/docker_run.sh
@@ -1,2 +1,2 @@
 docker build -t recoverpy-test-env .
-docker run recoverpy-test-env
+docker run recoverpy-test-env --tty
--- tests/docker_env/entrypoint.sh
+++ tests/docker_env/entrypoint.sh
@@ -2,6 +2,13 @@
 
 set -o errexit -o pipefail -o nounset
 
+### TODO ###
+#- Emulate a block device and mount it
+#- Add relevant python tests for this env
+############
+
 git clone https://github.com/PabloLec/recoverpy.git
 cd recoverpy
-python3 -m pip install -r requirements.txt
+python3 -m pip install . --use-feature=in-tree-build
+
+pytest
