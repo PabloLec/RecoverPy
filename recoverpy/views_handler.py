72f9e134c09c3ea623b55db717dc4679d5e9f86c
PabloLec <pablo.lecolinet@gmail.com>
2021-09-29 10:55:45 +0200

    Fix window switching bugs

--- recoverpy/views_handler.py
+++ recoverpy/views_handler.py
@@ -17,27 +17,30 @@ class ViewsHandler:
     def __init__(self):
         """Constructor for ViewsHandler."""
 
-        _parameters_view_window = None
-        _search_view_window = None
-        _results_view_window = None
+        self._parameters_view_window = None
+        self._search_view_window = None
+        self._results_view_window = None
 
     def open_view_parameters(self):
         """Start a ParametersView instance."""
 
         self._parameters_view_window = py_cui.PyCUI(10, 10)
         self._parameters_view_window.toggle_unicode_borders()
-        self._parameters_view_window.set_title("RecoverPy 1.3.1")
+        self._parameters_view_window.set_title("RecoverPy 1.3.2")
         _PARAMETERS_VIEW.ParametersView(self._parameters_view_window)
         self._parameters_view_window.start()
 
     def close_view_parameters(self):
         """Stop the ParametersView instance."""
 
+        if self._parameters_view_window is None:
+            return
         self._parameters_view_window.stop()
 
     def open_view_search(self, partition: str, string_to_search: str):
         """Start a SearchView instance."""
 
+        self.close_view_results()
         self._search_view_window = py_cui.PyCUI(10, 10)
         self._search_view_window.toggle_unicode_borders()
         self._search_view_window.set_title("View and explore found files")
@@ -51,11 +54,14 @@ class ViewsHandler:
     def close_view_search(self):
         """Stop the SearchView instance."""
 
+        if self._search_view_window is None:
+            return
         self._search_view_window.stop()
 
     def open_view_results(self, partition: str, block: str):
         """Start a ResultsView instance."""
 
+        self.close_view_search()
         self._results_view_window = py_cui.PyCUI(10, 10)
         self._results_view_window.toggle_unicode_borders()
         self._results_view_window.set_title("")
@@ -65,6 +71,18 @@ class ViewsHandler:
             initial_block=block,
         )
         self._results_view_window.start()
+
+    def close_view_results(self):
+        """Stop the ResultsView instance."""
+
+        if self._results_view_window is None:
+            return
+        self._results_view_window.stop()
+
+    def results_go_back(self):
+        self.close_view_results()
+        self._search_view_window._stopped = False
+        self._search_view_window.start()
 
 
 VIEWS_HANDLER = ViewsHandler()
