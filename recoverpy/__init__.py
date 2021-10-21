6fe3fadec68d9debead178cfd584efe1333267c5
PabloLec <pablo.lecolinet@gmail.com>
2021-10-21 10:22:40 +0200

    Check for mandatory deps at init

--- recoverpy/__init__.py
+++ recoverpy/__init__.py
@@ -2,6 +2,7 @@ from logging import getLogger
 from os import environ
 
 from recoverpy.config.config import load_config
+from recoverpy.utils.helper import is_installed
 from recoverpy.views_handler import VIEWS_HANDLER
 
 getLogger(__name__)
@@ -15,8 +16,23 @@ def verify_terminal_conf():
         environ["TERM"] = "xterm-256color"
 
 
+def verify_dependencies():
+    """Verify the availability of mandatory dependencies.
+
+    Raises:
+        OSError: If a dependency is missing.
+    """
+    for dep in ["grep", "dd", "lsblk"]:
+        if not is_installed(command=dep):
+            raise OSError(
+                f"Command '{dep}' is unavailable on your system.\n"
+                "Please verify your configuration."
+            )
+
+
 def main():
     """Set configuration and start UI."""
+    verify_dependencies()
     verify_terminal_conf()
     load_config()
 
