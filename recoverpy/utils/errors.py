b0d5899200fd24afe85ea277b09b700bb1a54213
PabloLec <pablo.lecolinet@gmail.com>
2022-03-21 16:11:12 +0100

    Remove useless docstrings from errors

--- recoverpy/utils/errors.py
+++ recoverpy/utils/errors.py
@@ -1,6 +1,4 @@
 class NoSavePath(Exception):
-    """Raised when save path is not set in config file."""
-
     def __init__(self):
         super().__init__(
             "No path entered for saving in config file. Verify your 'config.yaml' file."
@@ -8,8 +6,6 @@ class NoSavePath(Exception):
 
 
 class InvalidSavePath(Exception):
-    """Raised when save path is invalid."""
-
     def __init__(self):
         super().__init__(
             "Path entered for saving in config file is invalid. "
@@ -18,8 +14,6 @@ class InvalidSavePath(Exception):
 
 
 class InvalidLogPath(Exception):
-    """Raised when log path is invalid."""
-
     def __init__(self):
         super().__init__(
             "Path entered for logging in config file is invalid. "
