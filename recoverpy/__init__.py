da9f7203e1b54ee260d42e752eedad3154d9b933
PabloLec <pablo.lecolinet@gmail.com>
2022-12-10 11:50:07 +0100

    Add first CSS integration

--- recoverpy/__init__.py
+++ recoverpy/__init__.py
@@ -4,8 +4,10 @@ from ui.app import RecoverPyApp
 
 getLogger(__name__)
 
+
 def main() -> None:
     RecoverPyApp().run()
+
 
 if __name__ == "__main__":
     main()
--- recoverpy/ui/app.py
+++ recoverpy/ui/app.py
@@ -2,11 +2,13 @@ from textual.app import App
 
 from ui.screens.screen_search_params import SearchParamsScreen
 
+from ui.css import get_css
 
 
 class RecoverPyApp(App):
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
     SCREENS = {"params": SearchParamsScreen()}
+    CSS_PATH = get_css()
 
     def on_mount(self) -> None:
         self.push_screen("params")
