29901603d68ea4adee16685b0954f83a694542a2
PabloLec <pablo.lecolinet@gmail.com>
2022-12-17 16:15:50 +0100

    Add navigation buttons to result screen

--- recoverpy/ui/screens/screen_result.py
+++ recoverpy/ui/screens/screen_result.py
@@ -1,4 +1,6 @@
 from textual.app import ComposeResult
+from textual.containers import Horizontal
+from textual.events import Event
 from textual.screen import Screen
 from textual.widgets import Label, Button, TextLog
 
@@ -14,11 +16,11 @@ class ResultScreen(Screen):
         self._partition = ""
         self._block_size = 0
         self._inode = 0
-        self._inode_label = Label("")
+        self._inode_label = Label("", id="inode-label")
         self._block_content = TextLog(markup=False, wrap=True)
         super().__init__(*args, **kwargs)
 
-    def set(self, partition: str,  block_size: int, inode: int) -> None:
+    def set(self, partition: str, block_size: int, inode: int) -> None:
         self._partition = partition
         self._block_size = block_size
         self._inode = inode
@@ -30,12 +32,37 @@ class ResultScreen(Screen):
 
     def update_block_content(self) -> None:
         self._block_content.clear()
-        self._block_content.write(get_printable(decode_result(get_dd_output(self._partition, self._block_size, self._inode))))
+        self._block_content.write(
+            get_printable(decode_result(get_dd_output(self._partition, self._block_size, self._inode))))
 
     def compose(self) -> ComposeResult:
-        yield self._inode_label
-        yield self._block_content
-        yield Button("Go back", id="go-back-button")
+        yield Horizontal(self._inode_label, id="inode-label-container")
+        yield Horizontal(self._block_content, id="block-content-container")
+        yield Horizontal(Button("Go back", id="go-back-button"), id="go-back-button-container")
+        yield Horizontal(
+            Button("Previous", id="previous-button"),
+            Button("Add block", id="add-block-button"),
+            Button("Next", id="next-button"),
+            id="block-buttons-container"
+        )
+        yield Horizontal(
+            Button("Save", id="save-button"),
+            id="save-button-container"
+        )
 
-    async def on_button_pressed(self) -> None:
-        self.app.pop_screen()
+    async def on_button_pressed(self, event: Event) -> None:
+        button_id = event.sender.id
+        if button_id == "go-back-button":
+            self.app.pop_screen()
+        elif button_id == "previous-button":
+            self._inode -= 1
+            self.update_inode_label()
+            self.update_block_content()
+        elif button_id == "add-block-button":
+            pass
+        elif button_id == "next-button":
+            self._inode += 1
+            self.update_inode_label()
+            self.update_block_content()
+        elif button_id == "save-button":
+            pass
