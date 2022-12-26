06ea15a0af7aee0cf75eb357595322fbd557fa33
PabloLec <pablo.lecolinet@gmail.com>
2022-12-26 15:37:24 +0100

    Fix get_dd_output mock

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ def system_calls_mock(session_mocker):
     session_mocker.patch(
         "recoverpy.lib.search.thread_factory.start_grep_process", new=grep_output.start_grep_process
     )
-    session_mocker.patch("get_dd_output", return_value=dd_output.MOCK_DD_OUTPUT)
+    session_mocker.patch("recoverpy.lib.helper.get_dd_output", return_value=dd_output.MOCK_DD_OUTPUT)
     session_mocker.patch(
         "recoverpy.lib.lsblk._lsblk", return_value=lsblk_output.MOCK_LSBLK_OUTPUT
     )
