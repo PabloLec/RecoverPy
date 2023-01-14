69ca51e58ba004dd06bebe8d4cf0207530c36080
PabloLec <pablo.lecolinet@gmail.com>
2023-01-14 11:41:29 +0100

    Add tests to saver

--- tests/test_lsblk.py
+++ tests/test_lsblk.py
@@ -2,9 +2,8 @@ from recoverpy.lib.lsblk import get_partitions, _IGNORED_PARTITIONS_TYPES
 
 
 def test_get_partitions():
-    """Test lsblk."""
     partitions = get_partitions()
-    assert len(partitions) == 7
 
+    assert len(partitions) == 7
     for ignored_type in _IGNORED_PARTITIONS_TYPES:
         assert all(ignored_type not in partition.name for partition in partitions)
--- tests/test_saver.py
+++ tests/test_saver.py
@@ -1,8 +1,50 @@
+import os
+from os import chdir
+
 from recoverpy.lib.saver import Saver
+
+
+def test_add(tmp_path):
+    saver = Saver()
+
+    saver.add(1, "test1")
+    saver.add(2, "test2")
+    saver.add(0, "test0")
+
+    assert saver.get_selected_blocks_count() == 3
+
+
+def test_reset(tmp_path):
+    saver = Saver()
+
+    saver.add(1, "test1")
+    saver.add(2, "test2")
+    saver.add(0, "test0")
+    saver.reset()
+
+    assert saver.get_selected_blocks_count() == 0
+
+
+def test_update_save_path(tmp_path):
+    saver = Saver()
+
+    saver.update_save_path(str(tmp_path))
+    assert saver.save_path == tmp_path
+
+
+def test_get_selected_blocks_count(tmp_path):
+    saver = Saver()
+
+    saver.add(1, "test1")
+    saver.add(2, "test2")
+    saver.add(0, "test0")
+
+    assert saver.get_selected_blocks_count() == 3
 
 
 def test_save(tmp_path):
     saver = Saver()
+
     saver.update_save_path(str(tmp_path))
     saver.add(1, "test1")
     saver.add(2, "test2")
@@ -13,3 +55,12 @@ def test_save(tmp_path):
     with open(saver.last_saved_file, "r") as f:
         assert f.read().splitlines() == ["test0", "test1", "test2"]
 
+
+def test_save_no_path(tmp_path):
+    chdir(tmp_path)
+    saver = Saver()
+
+    saver.add(1, "test1")
+    saver.save()
+
+    assert saver.last_saved_file in list(tmp_path.iterdir())
